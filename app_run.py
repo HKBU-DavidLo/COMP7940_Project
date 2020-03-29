02a0a84c7b3ce47dd8afc6a7b7ca3cb37be19b7b
handsome168-web <56117925+handsome168-web@users.noreply.github.com>
2020-03-29 20:22:08 +0800

    Update app_run.py

--- app_run.py
+++ app_run.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 import os
+import redis
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
@@ -7,6 +8,13 @@ from linebot.models import MessageEvent, TextMessage, TextSendMessage
 from urllib.request import urlopen
 import configparser
 from linebot.models import *
+import re
+
+redis1 = redis.Redis(host = "redis-13333.c56.east-us.azure.cloud.redislabs.com", password = "ubZLeDUxIKCYKBHK15dtY3TjfnmPw824", port = "13333")
+redis1.set("symptoms","Fever,Cough,Shortness of breath or difficulty breathing,Tiredness,Aches,Runny nose and Sore throat.")
+redis1.set("protection","1,clean your hands for at least 20 seconds with soap and water, or use an alcohol-based sanitiser with at least 70% alcohol.2,cover your sneeze or cough with your elbow or with tissue.3,avoid close contact with people who are ill.4,avoid touching your eyes, nose and mouth.")
+redis1.set("risk factors","1,Recent travel from or residence in an area with ongoing community spread of COVID-19 as determined by CDC or WHO.2,Close contact with someone who has COVID-19 — such as when a family member or health care worker takes care of an infected person.")
+
 # from custom_models import ChannelTalks #, ChannelFlex, utils
 
 app = Flask(__name__)
@@ -36,17 +44,18 @@ def callback():
 
 # repeat text message
 @handler.add(MessageEvent, message=TextMessage)
+# import redis
 # list out all reply options:
 def reply_text_message(event):
     print(event)
     text = event.message.text
 
     if (text == "symptoms of COVID-19" or text == "symptoms"):
-        reply_text = "Fever,Cough,Shortness of breath or difficulty breathing,Tiredness,Aches,Runny nose and Sore throat"
+        reply_text = redis1.get("symptoms").decode('UTF-8')
     elif (text == "protection" or text == "precaution"):
-        reply_text = "1,clean your hands for at least 20 seconds with soap and water, or use an alcohol-based sanitiser with at least 70% alcohol.2,cover your sneeze or cough with your elbow or with tissue.3,avoid close contact with people who are ill.4,avoid touching your eyes, nose and mouth."
+        reply_text = redis1.get("protection").decode('UTF-8')
     elif (text == "risk factors"):
-        reply_text = "1,Recent travel from or residence in an area with ongoing community spread of COVID-19 as determined by CDC or WHO.2,Close contact with someone who has COVID-19 — such as when a family member or health care worker takes care of an infected person"
+        reply_text = redis1.get("risk factors").decode('UTF-8')
     elif (event.source.user_id != "Udeadbeefdfeadfsdlkfdasofjewa"):
         reply = False #not yet replied
 
