79f55735ca4de943c1c95134aa68433a38adec7a
handsome168-web <56117925+handsome168-web@users.noreply.github.com>
2020-04-12 23:13:58 +0800

    Update app_run.py

--- app_run.py
+++ app_run.py
@@ -67,7 +67,7 @@ def reply_text_message(event):
 
         #query key pandemic statistics in HK
         if re.match('number of cases', str(event.message.text).lower().strip()):
-            reply = ChannelTalks.epicdemic_record(event.decode('UTF-8'))
+            reply = ChannelTalks.epicdemic_record(event.decode('big5'))
             
         #trying reply by condition:
         if not reply:
