8e149142d8d24a2bdcfab7140ececd134c4ca571
ZHANGSHAO1212 <57306500+ZHANGSHAO1212@users.noreply.github.com>
2020-04-12 23:52:05 +0800

    new api

--- country.py
+++ country.py
@@ -1,15 +1,15 @@
 import http.client
-
-conn = http.client.HTTPSConnection("covid-19-data.p.rapidapi.com")
+import json
+conn = http.client.HTTPSConnection("covid-193.p.rapidapi.com")
 
 headers = {
-    'x-rapidapi-host': "covid-19-data.p.rapidapi.com",
+    'x-rapidapi-host': "covid-193.p.rapidapi.com",
     'x-rapidapi-key': "c292695aa2msh54c80405779f4a8p1695ddjsn47c37deef73b"
     }
-country=input()
-conn.request("GET", "/country?format=undefined&name="+country, headers=headers)
+country = input()
+conn.request("GET", "/statistics?country="+country, headers=headers)
 
 res = conn.getresponse()
 data = res.read()
-
-print(data.decode("utf-8"))
+content = json.loads(data)['response'][0]
+print(str(content['country'])+'\n'+'cases:'+str(content['cases'])+'\n'+'deaths:'+str(content['deaths'])+'\n'+'tests:'+str(content['tests'])+'\n'+'time:'+str(content['day']))
