aa16c065c9a5e730393adc177d5cbcb5bacdb7f1
handsome168-web <56117925+handsome168-web@users.noreply.github.com>
2020-04-12 23:43:38 +0800

    Update ChannelTalks.py

--- custom_models/ChannelTalks.py
+++ custom_models/ChannelTalks.py
@@ -77,7 +77,7 @@ def epicdemic_record(event):
     return True
 
 def scrape_manual(event):
-    scrape_success = utils.prepare_scrape().decode('UTF-8')
+    scrape_success = utils.prepare_scrape()
     return epicdemic_record(event)
 
 
